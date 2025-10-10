3798c475e36af9f0c369200ab90f5f82a0d389dd
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-10 14:17:38 +0900

    Update kappasha_os.py

--- kappasha_os.py
+++ kappasha_os.py
@@ -1,39 +1,38 @@
-#!/usr/bin/env python3
-# kappasha_os.py - Kappa-tilted OS with rhombus voxel navigation, factory sim integration.
-# CLI-driven, no GUI, DOS Navigator soul in 3D. Pure civilian engineering.
-# Dual License:
-# - For core software: AGPL-3.0-or-later licensed. -- OliviaLynnArchive fork, 2025
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#   GNU Affero General Public License for more details.
-#
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-# - For hardware/embodiment interfaces (if any): Licensed under the Apache License, Version 2.0
-#   with xAI amendments for safety (prohibits misuse in hashing; revocable for unethical use).
-#   See http://www.apache.org/licenses/LICENSE-2.0 for details.
-#
+# Copyright 2025 Beau Ayres, 
 # Copyright 2025 xAI
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
+# http://www.apache.org/licenses/LICENSE-2.0
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# SPDX-License-Identifier: Apache-2.0
+# Proprietary Software - All Rights Reserved
+#
+# This software is proprietary and confidential. Unauthorized copying,
+# distribution, modification, or use is strictly prohibited without
+# express written permission from Beau Ayres.
+#
+# AGPL-3.0-or-later licensed
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#!/usr/bin/env python3
+# kappasha_os.py - Kappa-tilted OS with rhombus voxel navigation, factory sim integration.
+# CLI-driven, no GUI, DOS Navigator soul in 3D. Pure civilian engineering.
 
 import simpy
 import numpy as np
