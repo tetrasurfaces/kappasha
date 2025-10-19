a04e5262b06ba875e09112000833178a99ce783b
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-20 01:11:23 +0900

    Update kappasha_os.py

--- kappasha_os.py
+++ kappasha_os.py
@@ -14,20 +14,15 @@
 # distribution, modification, or use is strictly prohibited without
 # express written permission from Beau Ayres.
 #
-# AGPL-3.0-or-later licensed
-#
+# Born Free. Feel Good. Have Fun.
+
+# Copyright 2025 Todd Macrae Hutchinson.
+# Licensed under GNU Affero General Public License v3.0 only
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, version 3.
+# No warranty. No wetware. Breath only.
+# Amendment: Biological use requires consent. Curve only. No bio hashes.
 
 #!/usr/bin/env python3
 # kappasha_os.py - Kappa-tilted OS with rhombus voxel navigation, factory sim integration.
